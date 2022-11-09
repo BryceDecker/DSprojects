00db7e13e3fe185a54f8af454c5c3a737256962a
BryceDecker <115592858+BryceDecker@users.noreply.github.com>
2022-11-09 15:50:53 -0600

    Create FizzBuzzPrompt.py

---
+++ intropythonproj/FizzBuzzPrompt.py
@@ -0,0 +1,7 @@
+#Programming with Mosh intro to python in an hour
+#Assignments taken from website
+#Second Assignment: Write a function that takes a number.
+#1. If number div by 3, return "Fizz"
+#2. If number div by 5, return "Buzz"
+#3. If number div by 3 and 5, return "FizzBuzz"
+#4. Otherwise, return the same number
