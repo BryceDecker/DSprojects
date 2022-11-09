441ad1b63511d67d224c72df3e583683d38b495d
BryceDecker <115592858+BryceDecker@users.noreply.github.com>
2022-11-09 16:01:55 -0600

    Create prompt.py

---
+++ intropythonproj/SpeedTrap/prompt.py
@@ -0,0 +1,6 @@
+# Programming with Mosh intro to python in an hour
+# Assignments taken from website
+# Third Assignment: Write a function for checking the speed of drivers. One parameter: speed
+# if speed < 70 print "Ok"
+# o/w every 5km over give driver 1 demerit point and print total points.
+# if demerit points > 12 print "License Suspended."
